b7aa740c59e71f7f25058ce321b9f75b4ded1f10
rustamali9900 <144158447+rustamali9900@users.noreply.github.com>
2026-01-24 13:40:05 +0500

    setting applayout and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,35 @@
+import { BrowserRouter, Navigate, Route, Routes } from "react-router";
+import GlobalStyles from "./styles/GlobalStyles";
+import PageNotFound from "./pages/PageNotFound";
+import Dashboard from "./pages/Dashboard";
+import Applayout from "./ui/Applayout";
+import Bookings from "./pages/Bookings";
+import Settings from "./pages/Settings";
+import Account from "./pages/Account";
+import Cabins from "./pages/Cabins";
+import Users from "./pages/Users";
+import Login from "./pages/Login";
 function App() {
-  return <div>Hello</div>;
+  return (
+    <>
+      <GlobalStyles />
+      <BrowserRouter>
+        <Routes>
+          <Route element={<Applayout />}>
+            <Route index element={<Navigate replace to="dashboard" />} />
+            <Route path="dashboard" element={<Dashboard />} />
+            <Route path="bookings" element={<Bookings />} />
+            <Route path="account" element={<Account />} />
+            <Route path="cabins" element={<Cabins />} />
+            <Route path="users" element={<Users />} />
+            <Route path="settings" element={<Settings />} />
+          </Route>
+          <Route path="login" element={<Login />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
---
+++ src/ui/Applayout.jsx
@@ -0,0 +1,5 @@
+function Applayout() {
+  return <div>Hell with you</div>;
+}
+
+export default Applayout;
