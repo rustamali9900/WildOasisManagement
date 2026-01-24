bc1abc4d0f5f7002287e9c44c05da452f37339a9
rustamali9900 <144158447+rustamali9900@users.noreply.github.com>
2026-01-24 14:24:17 +0500

    sidebar and navigation

--- src/ui/Applayout.jsx
+++ src/ui/Applayout.jsx
@@ -1,12 +1,30 @@
 import { Outlet } from "react-router";
+import Sidebar from "./Sidebar";
+import Header from "./Header";
+import styled from "styled-components";
 
-function Applayout() {
+const StyledAppLayout = styled.div`
+  display: grid;
+  grid-template-columns: 26rem 1fr;
+  grid-template-rows: auto 1fr;
+  height: 100vh;
+`;
+
+const Main = styled.main`
+  background-color: var(--color-grey-50);
+  padding: 4rem 4.8rem 6.4rem;
+`;
+
+function AppLayout() {
   return (
-    <div>
-      <p>Hell with you</p>
-      <Outlet />
-    </div>
+    <StyledAppLayout>
+      <Header />
+      <Sidebar />
+      <Main>
+        <Outlet />
+      </Main>
+    </StyledAppLayout>
   );
 }
 
-export default Applayout;
+export default AppLayout;
