631c524590c62b33c239b2d1f796245e77034274
rustamali9900 <144158447+rustamali9900@users.noreply.github.com>
2026-01-29 00:25:47 +0500

    Settings form setup

--- src/features/settings/UpdateSettingsForm.jsx
+++ src/features/settings/UpdateSettingsForm.jsx
@@ -1,21 +1,25 @@
-import Form from '../../ui/Form';
-import FormRow from '../../ui/FormRow';
-import Input from '../../ui/Input';
+import { useForm } from "react-hook-form";
+import Form from "../../ui/Form";
+import FormRow from "../../ui/FormRow";
+import Input from "../../ui/Input";
+import { useSettings } from "./useSettings";
 
 function UpdateSettingsForm() {
+  const { isPending, error, settings } = useSettings();
+  const { register } = useForm();
   return (
     <Form>
-      <FormRow label='Minimum nights/booking'>
-        <Input type='number' id='min-nights' />
+      <FormRow label="Minimum nights/booking">
+        <Input type="number" id="min-nights" defaultValue={}/>
       </FormRow>
-      <FormRow label='Maximum nights/booking'>
-        <Input type='number' id='max-nights' />
+      <FormRow label="Maximum nights/booking">
+        <Input type="number" id="max-nights" defaultValue={}/>
       </FormRow>
-      <FormRow label='Maximum guests/booking'>
-        <Input type='number' id='max-guests' />
+      <FormRow label="Maximum guests/booking">
+        <Input type="number" id="max-guests" defaultValue={}/>
       </FormRow>
-      <FormRow label='Breakfast price'>
-        <Input type='number' id='breakfast-price' />
+      <FormRow label="Breakfast price">
+        <Input type="number" id="breakfast-price" defaultValue={}/>
       </FormRow>
     </Form>
   );
