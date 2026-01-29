e8f11c431e805af863c1ec7ca6861ab69de85b58
rustamali9900 <144158447+rustamali9900@users.noreply.github.com>
2026-01-29 18:58:33 +0500

    Create AddCabin.jsx

---
+++ src/features/cabins/AddCabin.jsx
@@ -0,0 +1,27 @@
+import CreateCabinForm from "./CreateCabinForm";
+import Button from "../../ui/Button";
+import Modal from "../../ui/Modal";
+import { useState } from "react";
+
+function AddCabin() {
+  const [isOpenModal, setOpenModal] = useState(false);
+
+  return (
+    <>
+      <Button
+        $variation="primary"
+        $size="medium"
+        onClick={() => setOpenModal((form) => !form)}
+      >
+        Add Cabin
+      </Button>
+
+      {isOpenModal && (
+        <Modal onClose={() => setOpenModal(false)}>
+          <CreateCabinForm />
+        </Modal>
+      )}
+    </>
+  );
+}
+export default AddCabin;
