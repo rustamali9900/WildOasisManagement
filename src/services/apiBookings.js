5fac1ec9b8bec590424f600aa9495ffbbd24a081
rustamali9900 <144158447+rustamali9900@users.noreply.github.com>
2026-01-30 21:24:13 +0500

    Started with Bookings Page

--- src/services/apiBookings.js
+++ src/services/apiBookings.js
@@ -1,6 +1,16 @@
 import { getToday } from "../utils/helpers";
 import supabase from "./supabase";
 
+export async function getBookings() {
+  const { data, error } = await supabase.from("bookings").select("*");
+
+  if (error) {
+    console.error(error);
+    throw new Error("Bookings could not be loaded");
+  }
+
+  return data;
+}
 export async function getBooking(id) {
   const { data, error } = await supabase
     .from("bookings")
@@ -55,7 +65,7 @@ export async function getStaysTodayActivity() {
     .from("bookings")
     .select("*, guests(fullName, nationality, countryFlag)")
     .or(
-      `and(status.eq.unconfirmed,startDate.eq.${getToday()}),and(status.eq.checked-in,endDate.eq.${getToday()})`
+      `and(status.eq.unconfirmed,startDate.eq.${getToday()}),and(status.eq.checked-in,endDate.eq.${getToday()})`,
     )
     .order("created_at");
 
