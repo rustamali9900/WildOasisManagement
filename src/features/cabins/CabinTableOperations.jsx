a0a9c65aa26d03ede5f2a3fb19acea1508c3676c
rustamali9900 <144158447+rustamali9900@users.noreply.github.com>
2026-01-30 20:42:47 +0500

    Adding sort by

--- src/features/cabins/CabinTableOperations.jsx
+++ src/features/cabins/CabinTableOperations.jsx
@@ -1,10 +1,18 @@
 import TableOperations from "../../ui/TableOperations";
 import Filter from "../../ui/Filter";
+import SortBy from "../../ui/SortBy";
 
 function CabinTableOperations() {
   return (
     <TableOperations>
       <Filter />
+      <SortBy
+        options={[
+          { value: "name-asc", label: "Sort by Name" },
+          { value: "price-asc", label: "Sort by Price" },
+          { value: "maxCapacity-asc", label: "Sort by Capacity" },
+        ]}
+      />
     </TableOperations>
   );
 }
