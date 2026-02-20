7105d77a35d03cfee7647feb79aa8e6619353694
rustamali9900 <144158447+rustamali9900@users.noreply.github.com>
2026-02-20 05:27:41 +0500

    user signup

--- src/features/authentication/SignupForm.jsx
+++ src/features/authentication/SignupForm.jsx
@@ -1,35 +1,45 @@
 import { useForm } from "react-hook-form";
-import Button from "../../ui/Button";
-import Form from "../../ui/Form";
+import { useSignup } from "./useSignup";
 import FormRow from "../../ui/FormRow";
+import Button from "../../ui/Button";
 import Input from "../../ui/Input";
+import Form from "../../ui/Form";
 
 function SignupForm() {
-  const { register, formState, getValues, handleSubmit } = useForm();
+  const { signup, isPending } = useSignup();
+  const { register, formState, getValues, handleSubmit, reset } = useForm();
   const { errors } = formState;
 
-  function onSubmit(data) {
-    console.log(data);
+  function onSubmit({ fullName, email, password }) {
+    signup(
+      { fullName, email, password },
+      {
+        onSettled: () => reset(),
+      },
+    );
   }
+
   return (
     <Form onSubmit={handleSubmit(onSubmit)}>
-      <FormRow label="Full name" error={errors?.fullName?.message}>
+      <FormRow label="Full name" errors={errors?.fullName?.message}>
         <Input
           type="text"
           id="fullName"
+          disabled={isPending}
           {...register("fullName", { required: "This field is required" })}
         />
       </FormRow>
 
-      <FormRow label="Email address" error={errors?.email?.message}>
+      <FormRow label="Email address" errors={errors?.email?.message}>
         <Input
           type="email"
           id="email"
+          disabled={isPending}
           {...register("email", {
             required: "This field is required",
             pattern: {
               value: /\S+@\S+\.\S+/,
-              message: "Please provide a valid Email Address",
+              message: "Please provide a valid email address",
             },
           })}
         />
@@ -37,38 +47,49 @@ function SignupForm() {
 
       <FormRow
         label="Password (min 8 characters)"
-        error={errors?.password?.message}
+        errors={errors?.password?.message}
       >
         <Input
           type="password"
           id="password"
+          disabled={isPending}
           {...register("password", {
             required: "This field is required",
             minLength: {
               value: 8,
-              message: "Minimum password length is 8",
+              message: "Password needs a minimum of 8 characters",
             },
           })}
         />
       </FormRow>
 
-      <FormRow label="Repeat password" error={errors?.passwordConfirm?.message}>
+      <FormRow
+        label="Repeat password"
+        errors={errors?.passwordConfirm?.message}
+      >
         <Input
           type="password"
           id="passwordConfirm"
+          disabled={isPending}
           {...register("passwordConfirm", {
             required: "This field is required",
             validate: (value) =>
-              value === getValues().password || "The passwords do not match",
+              value === getValues().password || "Passwords need to match",
           })}
         />
       </FormRow>
 
       <FormRow>
-        <Button $variation="secondary" $size="medium" type="reset">
+        <Button
+          $variation="secondary"
+          $size="medium"
+          type="reset"
+          disabled={isPending}
+          onClick={reset}
+        >
           Cancel
         </Button>
-        <Button $variation="primary" $size="medium">
+        <Button $variation="primary" $size="medium" disabled={isPending}>
           Create new user
         </Button>
       </FormRow>
---
+++ src/features/authentication/useSignup.js
@@ -0,0 +1,16 @@
+import { signup as signupApi } from "../../services/apiAuth";
+import { useMutation } from "@tanstack/react-query";
+import { toast } from "react-hot-toast";
+
+export function useSignup() {
+  const { mutate: signup, isLoading } = useMutation({
+    mutationFn: signupApi,
+    onSuccess: () => {
+      toast.success(
+        "Account successfully created! Please verify the new account from the user's email address.",
+      );
+    },
+  });
+
+  return { signup, isLoading };
+}
