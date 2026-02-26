64fe93162f889d4d3689801798b3813ced14c8af
rustamali9900 <144158447+rustamali9900@users.noreply.github.com>
2026-02-27 03:57:02 +0500

    commiting some changes

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import checker from "vite-plugin-checker";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    checker({
-      // We use 'eslint' because your package.json has eslint 9.x
-      eslint: {
-        // Tells the plugin to use the modern "flat" config system
-        useFlatConfig: true,
-        // The exact command to run the check
-        lintCommand: 'eslint "./src/**/*.{js,jsx}"',
-      },
-      // This prevents the "red screen" from blocking your view in dev
-      overlay: {
-        initialIsOpen: false,
-      },
-    }),
-  ],
+  plugins: [react()],
 });
